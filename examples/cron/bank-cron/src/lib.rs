45f88df92477a71dfef705c987a84e88fa61981c
nhannamsiu <dangduynam95@gmail.com>
2024-06-10 21:49:40 +0700

    feat: add msg input for cron example

--- examples/cron/bank-cron/src/lib.rs
+++ examples/cron/bank-cron/src/lib.rs
@@ -1,8 +1,5 @@
 use cosmwasm_schema::cw_serde;
-use cosmwasm_std::{
-    entry_point, to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo,
-    Response, StdResult,
-};
+use cosmwasm_std::{entry_point, to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, from_json};
 use std::{vec::Vec};
 
 #[cw_serde]
@@ -23,8 +20,10 @@ pub struct FisInput {
 }
 
 #[cw_serde]
-pub struct Fund {
-    receivers: Vec<String>,
+pub struct CronInput {
+    receiver: String,
+    amount: String,
+    denom: String,
 }
 
 #[cw_serde]
@@ -74,21 +73,24 @@ pub fn execute(
 }
 
 #[entry_point]
-pub fn query(_deps: Deps, env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+pub fn query(_deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    // parse cron input
+    let command = from_json::<CronInput>(msg.msg)?;
+
     // parse command, we can store it as proto bytes, encrypted binary
     let mut instructions = vec![];
 
-    // send 1 usdt
+    // send usdt
     instructions.push(FISInstruction {
         plane: "COSMOS".to_string(),
         action: "COSMOS_BANK_SEND".to_string(),
         address: "".to_string(),
         msg: to_json_binary(&MsgSend {
             from_address: env.contract.address.clone().into_string(),
-            to_address: "lux158ucxjzr6ccrlpmz8z05wylu8tr5eueqcp2afu".to_string(), // wasm account
+            to_address: command.receiver.to_string(),
             amount: vec![BankAmount {
-                denom: "usdt".to_string(),
-                amount: "1".to_string(),
+                denom: command.denom.to_string(),
+                amount: command.amount.to_string(),
             }],
         })
         .unwrap()
