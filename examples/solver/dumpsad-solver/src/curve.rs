d1071b342497ab7dfc746af8a13a1a37e7d8ea53
vinhphuctadang <vinhphuctadang@gmail.com>
2024-12-16 17:40:02 +0700

    fix: sell logic

--- examples/solver/dumpsad-solver/src/curve.rs
+++ examples/solver/dumpsad-solver/src/curve.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::Uint128;
 
-// curve y = a*1_000_000_000 - (b*1_000_000_000) / (30 * 1_000_000_000 + x)
+// curve y = a*1000_000_000 - (b*1000_000_000) / (30 * 1_000_000_000 + x)
 // both SOL and meme has 9 decimals
 // where: y: total meme minted, x: total sol deposited
 pub struct BondingCurve {
@@ -9,6 +9,9 @@ pub struct BondingCurve {
     pub x: Uint128,
     pub y: Uint128,
 }
+
+// 67062511 = 1073000191-32190005730/(32)
+// 32190005730/(1073000191 - 50000000)
 
 impl BondingCurve {
     const PRECISION_MULTIPLIER: Uint128 = Uint128::new(1_000_000_000u128);
@@ -32,7 +35,10 @@ impl BondingCurve {
 
     // dY = delta Y, dX = delta X
     pub fn buy(&mut self, dx: Uint128) -> Uint128 {
-        // dY = y - a + b / (30 + x + dX)
+        // y = a - b / (30 + x) (recall: y is the minted amount for user, counting from 0, not 10^9)
+        // newY = a - b / (30 + newX)
+        // newX = x + dx
+        // newY = y + dy => dy = newY - y
         let new_x = Uint128::new(30) * BondingCurve::PRECISION_MULTIPLIER + self.x + dx;
         let new_y = self.a - (self.b * BondingCurve::PRECISION_MULTIPLIER) / new_x;
         let dy = new_y - self.y;
@@ -45,12 +51,14 @@ impl BondingCurve {
     }
 
     pub fn sell(&mut self, dy: Uint128) -> Uint128 {
-        // dX = 30 + x - b / (y - dY - a)
-        let new_y = self.y - dy;
-        let new_x = Uint128::new(30) * BondingCurve::PRECISION_MULTIPLIER + self.x
-            - (self.b * BondingCurve::PRECISION_MULTIPLIER) / (self.a - new_y);
-        let dx = self.x - new_x;
-
+        // y = a - b / (30 + x) (recall: y is the minted amount for user, counting from 0, not 10^9)
+        // newY = a - b / (30 + newX)
+        // newY = y - dy
+        // newX = x - dx => dx = x - newX
+        let new_y = self.y - dy; // (10^9*10^9 - 932937488062500000 - 10000000000000) = 67052511937500000
+        let new_x = (self.b * BondingCurve::PRECISION_MULTIPLIER) / (self.a - new_y); // 32190005730*10^9*10^9 / (1073000191*10^9 - 67052511937500000)
+                                                                                      // new_x = 30+x-dx => dx = 30+x-new_x
+        let dx = Uint128::new(30) * BondingCurve::PRECISION_MULTIPLIER + self.x - new_x;
         // Update state
         self.x = new_x;
         self.y = new_y;
